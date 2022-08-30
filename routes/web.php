9f32a88484613b5ae7fba0901ae3e5839b42d38f
Aurorah Harmony <aurorahHarmony@gmail.com>
2022-08-30 20:27:32 +1200

    Redirect to login on home route not authenticated

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,12 @@
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::middleware('guest')->group(function() {
-    Route::get('/login', [LoginController::class, 'create']);
+    Route::get('/login', [LoginController::class, 'create'])->name('login');
 });
 
-Route::get('/', function () {
-    return inertia('welcome');
+Route::middleware('auth')->group(function() {
+    Route::get('/', function () {
+        return inertia('welcome');
+    });
 });

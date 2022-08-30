29eced903d83a427f2d3d63b69e3a794349f4a5a
Aurorah Harmony <aurorahHarmony@gmail.com>
2022-08-30 23:06:29 +1200

    Install font awesome

--- resources/scripts/main.ts
+++ resources/scripts/main.ts
@@ -2,6 +2,13 @@ import { createApp, h } from 'vue';
 import { createInertiaApp, Head, Link } from '@inertiajs/inertia-vue3';
 import { resolvePageComponent } from 'vite-plugin-laravel/inertia';
 
+// Import icons.
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faHouse, faQrcode } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faHouse, faQrcode);
+
 createInertiaApp({
     resolve: (name) =>
         resolvePageComponent(name, import.meta.glob('../views/pages/**/*.vue')),
@@ -10,6 +17,7 @@ createInertiaApp({
             .use(plugin)
             .component('Head', Head)
             .component('Link', Link)
+            .component('font-awesome-icon', FontAwesomeIcon)
             .mount(el);
     },
     title: (title) => 'Shyft' + (title.trim() ? ` | ${title}` : ''),

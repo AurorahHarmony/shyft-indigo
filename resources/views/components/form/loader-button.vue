c58e8113045c39eec4e959d8016bb5ac321bfccc
Aurorah Harmony <aurorahHarmony@gmail.com>
2022-09-02 22:08:18 +1200

    Create loader button component

---
+++ resources/views/components/form/loader-button.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  loading: { type: Boolean, default: false },
+});
+</script>
+<template>
+  <button
+    class="w-100 btn"
+    :class="$attrs.class ? $attrs.class : 'btn-primary'"
+    :disabled="loading"
+  >
+    <span v-if="loading">
+      <span
+        class="spinner-border spinner-border-sm"
+        role="status"
+        aria-hidden="true"
+      ></span>
+      Loading
+    </span>
+    <span v-else><slot /></span>
+  </button>
+</template>

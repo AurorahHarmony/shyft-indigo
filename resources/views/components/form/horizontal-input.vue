006a105788c92a2860ce015a8961e2a3aac35fd4
Aurorah Harmony <aurorahHarmony@gmail.com>
2022-09-03 16:49:32 +1200

    Emit edit event

--- resources/views/components/form/horizontal-input.vue
+++ resources/views/components/form/horizontal-input.vue
@@ -8,31 +8,36 @@ const props = defineProps({
   error: { type: Boolean, default: false },
 });
 
-const emit = defineEmits(['update:modelValue']);
+const emit = defineEmits(['update:modelValue', 'enableEdit']);
 
 const handleInput = (e) => {
   emit('update:modelValue', e.target.value);
 };
 </script>
 <template>
-  <div class="row mb-2">
-    <label :for="id" class="col-sm-3 col-form-label">
+  <div class="row mb-2 g-0">
+    <label class="col-sm-3 col-form-label">
       {{ label }}
     </label>
-    <div class="col-sm-8">
+    <div class="col-11 col-sm-8">
       <input
         :type="type"
         class="form-control"
         :class="{ 'is-invalid': error }"
-        :id="id"
         :disabled="disabled == 1"
         :value="modelValue"
         @input="handleInput"
       />
       <div class="invalid-feedback" v-if="error">{{ error }}</div>
     </div>
-    <div class="col-1 d-flex align-items-center">
-      <a href="#" class="text-decoration-none" v-if="canEdit">Edit</a>
+    <div class="col-1 d-flex align-items-center justify-content-center px-1">
+      <a
+        href="#"
+        class="text-decoration-none"
+        v-if="canEdit"
+        @click="emit('enableEdit')"
+        >Edit</a
+      >
     </div>
   </div>
 </template>
